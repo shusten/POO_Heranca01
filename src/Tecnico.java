849f82f28bee0c76249808c585d47d2bc2c310b2
Luis Villas <shusten@hotmail.com>
2019-12-11 01:10:39 -0300

    Add classe Tecnico

---
+++ src/Tecnico.java
@@ -0,0 +1,7 @@
+public class Tecnico extends Aluno{
+    private int registro;
+
+    public void praticar(){
+        System.out.println("Aluno técnico " + this.getNome() + "praticando!");
+    }
+}
